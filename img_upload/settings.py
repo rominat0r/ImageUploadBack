3d250e85d92d2cd52d73b5011f9969f11a854bfe
roman <roman@pop-os.localdomain>
2022-06-28 01:26:48 +0300

    anoh

--- img_upload/settings.py
+++ img_upload/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'img_upload/mediafiles')
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static/mediafiles')
 
 # URL used to access the media
 MEDIA_URL = '/media/'
